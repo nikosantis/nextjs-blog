3be79dbc4d1f4de52185875e39b0f6429015b35e
Nikolas Santis <nikosantis@gmail.com>
2020-05-27 21:40:48 -0400

    scss to css

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import styles from './layout.module.scss'
+import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
